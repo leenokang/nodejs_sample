4fb31eb2c7578b663249775562c3f29d781046d0
leenokang <leenokang@gmail.com>
2020-03-29 16:58:49 +0900

    deploy_4
    Signed-off-by: leenokang <leenokang@gmail.com>

---
+++ main.js
@@ -0,0 +1,106 @@
+var http =require('http');
+var fs = require('fs');
+var url = require('url');
+
+function templateHTML(title, list, body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="./style.css">
+  </head>
+  <body>
+    <div class="container">
+      <header>
+        <h1><a id=text href="/">AWS SDK Exercise</a></h1>
+      </header>
+      <section class="content">
+        ${list}
+        ${body}
+      </section>
+      <footer>
+        <h2> "Lucky Number : ${Lucky}" </h2>
+      </footer>
+    </div>
+  </body>
+  </html>
+  `;
+}
+
+function lottoNum () {
+  let lotto = [];
+  let i=0;
+  while ( i<6 ) {
+    let n = Math.floor(Math.random() * 45) + 1;
+    if (! sameNum(n)) {
+      lotto.push(n);
+      i++;
+    }
+}
+  function sameNum (n) {
+     for (var i=0; i < lotto.length; i++) {
+        if (n === lotto[i]){
+          return true;
+        }
+    }
+    return false
+  }
+  return lotto;
+}
+
+var Lucky = lottoNum()
+
+function templateList(filelist){
+  var list = '<ul>';
+  var i = 0;
+  while(i < filelist.length){
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
+  }
+  list = list+'</ul>';
+  return list;
+}
+
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+    if(pathname === '/'){
+      if(queryData.id === undefined){
+        fs.readdir('./data', function(error, filelist){
+          var title = 'Welcome';
+          var description = 'Hello, let us start';
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `
+            <div class="article">
+              <h3>${title}</h3>
+                ${description}
+            </div>
+            `);
+          response.writeHead(200);
+          response.end(template);
+        })
+      } else {
+        fs.readdir('./data', function(error, filelist){
+          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+            var title = queryData.id;
+            var list = templateList(filelist);
+            var template = templateHTML(title, list, `
+              <div class="article">
+                <h3>${title}</h3>
+                  ${description}
+              </div>
+              `);
+            response.writeHead(200);
+            response.end(template);
+          });
+        });
+      }
+    } else {
+      response.writeHead(404);
+      response.end('Not found');
+    }
+});
+app.listen(8080);
---
+++ main_old.js
@@ -0,0 +1,76 @@
+var http =require('http');
+var fs = require('fs');
+var url = require('url');
+
+function templateHTML(title, list, body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+    <style type="text/css">
+      h1 {
+         border-bottom:1px solid;
+         padding:20px;
+         backgroud-color:#cddc39;
+         }
+      ul {
+         border-right:1px;
+         width:200px;
+         height:400px;
+         float:left;
+         }
+    </style>
+  </head>
+  <body>
+    <h1><a href="/">AWS SDK Exercise</a></h1>
+    ${list}
+    ${body}
+    </body>
+  </html>
+  `;
+}
+
+function templateList(filelist){
+  var list = '<ul>';
+  var i = 0;
+  while(i < filelist.length){
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
+  }
+  list = list+'</ul>';
+  return list;
+}
+
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+    if(pathname === '/'){
+      if(queryData.id === undefined){
+        fs.readdir('./data', function(error, filelist){
+          var title = 'Welcome';
+          var description = 'Hello, let us start';
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+          response.writeHead(200);
+          response.end(template);
+        })
+      } else {
+        fs.readdir('./data', function(error, filelist){
+          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+            var title = queryData.id;
+            var list = templateList(filelist);
+            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+            response.writeHead(200);
+            response.end(template);
+          });
+        });
+      }
+    } else {
+      response.writeHead(404);
+      response.end('Not found');
+    }
+});
+app.listen(8080);
