8080ed46d1ca9c5ddfcdf8a0bb11ab193e0bd6b9
leenokang <leenokang@gmail.com>
2020-04-05 13:45:59 +0900

    20200405_deploy

--- main.js
+++ main.js
@@ -1,135 +1,146 @@
-var http =require('http');
+var express = require('express')
+var app = express()
 var fs = require('fs');
-var url = require('url');
+var path = require('path');
+var qs = require('querystring');
+var bodyParser = require('body-parser');
+var sanitizeHtml = require('sanitize-html');
+var compression = require('compression')
+var template = require('./lib/template.js');
+var helmet = require('helmet')
+app.use(helmet());
 
-function templateHTML(title, list, body){
-  return `
-  <!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-    <style>
-      .container {
-        display:flex;
-        flex-direction:column;
-      }
-      header {
-        border-bottom:1px solid gray;
-        padding-left:20px;
-      }
-      #text {
-        text-decoration:none;
-      }
-      .content {
-        display:flex;
-        flex-direction:raw;
-      }
-      .content ul {
-          border-right:1px;
-          flex-basis:200px;
-          flex-shrink:0;
-          list-style:none;
-          float:left;
-      }
-      footer {
-        border-top:1px;
-        padding-left:20px;
-        text-align:center;
-        background-color:lightgray;
-      }
-    </style>
-  </head>
-  <body>
-    <div class="container">
-      <header>
-        <h1><a id=text href="/">AWS SDK Exercise</a></h1>
-      </header>
-      <section class="content">
-        ${list}
-        ${body}
-      </section>
-      <footer>
-        <h2> "Lucky Number : ${Lucky}" </h2>
-      </footer>
-    </div>
-  </body>
-  </html>
-  `;
-}
-
-function lottoNum () {
-  let lotto = [];
-  let i=0;
-  while ( i<6 ) {
-    let n = Math.floor(Math.random() * 45) + 1;
-    if (! sameNum(n)) {
-      lotto.push(n);
-      i++;
-    }
-}
-  function sameNum (n) {
-     for (var i=0; i < lotto.length; i++) {
-        if (n === lotto[i]){
-          return true;
-        }
-    }
-    return false
-  }
-  return lotto;
-}
-
-var Lucky = lottoNum()
-
-function templateList(filelist){
-  var list = '<ul>';
-  var i = 0;
-  while(i < filelist.length){
-    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-    i = i + 1;
-  }
-  list = list+'</ul>';
-  return list;
-}
-
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    if(pathname === '/'){
-      if(queryData.id === undefined){
-        fs.readdir('./data', function(error, filelist){
-          var title = 'Welcome';
-          var description = 'Hello, let us start';
-          var list = templateList(filelist);
-          var template = templateHTML(title, list, `
-            <div class="article">
-              <h3>${title}</h3>
-                ${description}
-            </div>
-            `);
-          response.writeHead(200);
-          response.end(template);
-        })
-      } else {
-        fs.readdir('./data', function(error, filelist){
-          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-            var title = queryData.id;
-            var list = templateList(filelist);
-            var template = templateHTML(title, list, `
-              <div class="article">
-                <h3>${title}</h3>
-                  ${description}
-              </div>
-              `);
-            response.writeHead(200);
-            response.end(template);
-          });
-        });
-      }
-    } else {
-      response.writeHead(404);
-      response.end('Not found');
-    }
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compression());
+app.get('*', function(request, response, next){
+  fs.readdir('./data', function(error, filelist){
+    request.list = filelist;
+    next();
+  });
 });
-app.listen(8080);
+
+//route, routing
+//app.get('/', (req, res) => res.send('Hello World!'))
+//<img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px;">
+app.get('/', function(request, response) {
+  var title = 'Welcome';
+  var description = 'Hello, Node.js';
+  var list = template.list(request.list);
+  var html = template.HTML(title, list,
+    `<h2>${title}</h2>${description}`,
+    `<a href="/create">create</a>`
+  );
+  response.send(html);
+});
+
+app.get('/page/:pageId', function(request, response, next) {
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+    if(err){
+      next(err);
+    } else {
+      var title = request.params.pageId;
+      var sanitizedTitle = sanitizeHtml(title);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags:['h1']
+      });
+      var list = template.list(request.list);
+      var html = template.HTML(sanitizedTitle, list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/create">create</a>
+          <a href="/update/${sanitizedTitle}">update</a>
+          <form action="/delete_process" method="post">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
+            <input type="submit" value="delete">
+          </form>`
+      );
+      response.send(html);
+    }
+  });
+});
+
+app.get('/create', function(request, response){
+  var title = 'WEB - create';
+  var list = template.list(request.list);
+  var html = template.HTML(title, list, `
+    <form action="/create_process" method="post">
+      <p><input type="text" name="title" placeholder="title"></p>
+      <p>
+        <textarea name="description" placeholder="description"></textarea>
+      </p>
+      <p>
+        <input type="submit">
+      </p>
+    </form>
+  `, '');
+  response.send(html);
+});
+
+app.post('/create_process', function(request, response){
+  var post = request.body;
+  var title = post.title;
+  var description = post.description;
+  fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+    response.writeHead(302, {Location: `/?id=${title}`});
+    response.end();
+  });
+});
+
+app.get('/update/:pageId', function(request, response){
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+    var title = request.params.pageId;
+    var list = template.list(filelist);
+    var html = template.HTML(title, list,
+      `
+      <form action="/update_process" method="post">
+        <input type="hidden" name="id" value="${title}">
+        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+        <p>
+          <textarea name="description" placeholder="description">${description}</textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+      </form>
+      `,
+      `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+    );
+    response.send(html);
+  });
+});
+
+app.post('/update_process', function(request, response){
+  var post = request.body;
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+  fs.rename(`data/${id}`, `data/${title}`, function(error){
+    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+      response.redirect(`/?id=${title}`);
+    })
+  });
+});
+
+app.post('/delete_process', function(request, response){
+  var post = request.body;
+  var id = post.id;
+  var filteredId = path.parse(id).base;
+  fs.unlink(`data/${filteredId}`, function(error){
+    response.redirect('/');
+  });
+});
+
+app.use(function(req, res, next) {
+  res.status(404).send('Sorry cant find that!');
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+});
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!')
+});
